e882292e5331e411246222b8859b043466acac89
johnnyzhang <1113638674@qq.com>
2017-11-06 10:44:11 +0800

    init

---
+++ config/laraadmin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Config genrated using LaraAdmin
+ * Help: http://laraadmin.com
+ */
+
+return [
+    
+	/*
+    |--------------------------------------------------------------------------
+    | General Configuration
+    |--------------------------------------------------------------------------
+    */
+    
+	'adminRoute' => 'admin',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Uploads Configuration
+    |--------------------------------------------------------------------------
+    |
+    | private_uploads: Show that uploaded file remains private and can be seen by respective owners only
+    | default_uploads_security: public / private
+    | 
+    */
+    'uploads' => [
+        'private_uploads' => false,
+        'default_public' => false,
+        'allow_filename_change' => true
+    ],
+];
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+});
+// var elixir = require('laravel-elixir');
+
+elixir(function(mix) {
+    mix.less('admin-lte/AdminLTE.less', 'public/la-assets/css');
+    mix.less('bootstrap/bootstrap.less', 'public/la-assets/css');
+});
+
+/*
+var minify = require('gulp-minify');
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
+*/
