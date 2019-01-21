6b823053241412ac8c7610f6a1b4cb65146d702e
johnnyzhang1992 <zq19920306@gmail.com>
2019-01-21 15:54:05 +0800

    微调

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,4 +1,12 @@
 <footer class="footer">
+    <div class="xcx-image clearfix" style="margin-bottom: 15px">
+        <div class="col-md-6 col-xs-6" style="padding-left: 0">
+            <img src="https://xuegushi.cn/static/common/xcx.jpg" class="img-responsive" />
+        </div>
+        <div class="col-md-6 col-xs-6" style="padding-right: 0">
+            <img src="https://xuegushi.cn/static/common/wechat.jpg" class="img-responsive" />
+        </div>
+    </div>
     {{--<a href="#" class="footer-item" target="_blank">关于</a>--}}
     {{--<span class="footer-dot">&sdot;</span>--}}
     <a href="{{ url('join') }}" class="footer-item" target="_blank">加入</a>
@@ -11,9 +19,9 @@
     {{--<span class="footer-dot">&sdot;</span>--}}
     {{--<a href="{{ url('donate-us') }}" class="footer-item">捐赠</a>--}}
     <br>
-    <a type="button" title="微信订阅号(xuegushiwen)" class="footer-item wechat-qrcode" style="cursor: pointer">微信公众号</a>
-    <span class="footer-dot">&sdot;</span>
-    <a type="button" title="小程序(古诗文小助手)" class="footer-item wxxcx-qrcode" style="cursor: pointer">小程序</a>
-    <br>
+    {{--<a type="button" title="微信订阅号(xuegushiwen)" class="footer-item wechat-qrcode" style="cursor: pointer">微信公众号</a>--}}
+    {{--<span class="footer-dot">&sdot;</span>--}}
+    {{--<a type="button" title="小程序(古诗文小助手)" class="footer-item wxxcx-qrcode" style="cursor: pointer">小程序</a>--}}
+    {{--<br>--}}
     <span class="footer-item">&copy; 2017 学古诗网</span>
 </footer>
