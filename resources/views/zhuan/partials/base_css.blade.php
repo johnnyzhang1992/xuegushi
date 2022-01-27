10bd2ebf750c8c4a646fb40c67783839023375a6
18238836800 <18238836800@126.com>
2022-01-27 14:19:12 +0800

    update css resource

--- resources/views/zhuan/partials/base_css.blade.php
+++ resources/views/zhuan/partials/base_css.blade.php
@@ -1,6 +1,6 @@
 <!-- Bootstrap core CSS -->
-{{--<link href="{{ asset('/la-assets/css/bootstrap.css') }}" rel="stylesheet">--}}
-<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-{{--<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />--}}
-<link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/la-assets/css/bootstrap.css') }}" rel="stylesheet">
+{{--<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"> --}}
+<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+{{--<link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />--}}
 <link rel="stylesheet" href="{{ asset(elixir('css/zhuanlan.css')) }}">
